6884fb04263f5ba57e27638e6541d0a2a1f022ef
neelfirst <neelfirst@gmail.com>
2021-12-26 19:08:15 -0800

    initial ev script

---
+++ summon_ev.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+
+import json, pandas, argparse
+
+HERO_FILE = 'gen_data.txt'
+def load_data(FILE):
+  with open(FILE,'r') as f:
+    df = pandas.DataFrame(json.loads(f.read()))
+  return df
+HEROES = load_data(HERO_FILE)
+CLASSES = ['warrior', 'knight', 'archer', 'thief', 'pirate', 'monk', 'wizard', 'priest', 'paladin', 'darkKnight', 'ninja', 'summoner']
+PROFESSIONS = ['mining', 'gardening', 'foraging', 'fishing']
+
+def gen1_floor(c, p):
+  if p == 'mining' and (c == 'warrior' or c == 'knight'):
+    return 150
+  if p == 'mining' and (c == 'archer' or c == 'thief'):
+    return 100
+  if p == 'mining' and (c == 'pirate' or c == 'monk'):
+    return 80
+  if p == 'mining' and (c == 'wizard' or c == 'priest'):
+    return 70
+  if p == 'gardening' and (c == 'warrior' or c == 'knight'):
+    return 100
+  if p == 'gardening' and (c == 'archer' or c == 'thief'):
+    return 100
+  if p == 'gardening' and (c == 'pirate' or c == 'monk'):
+    return 75
+  if p == 'gardening' and (c == 'wizard' or c == 'priest'):
+    return 150
+  if p == 'foraging' and (c == 'warrior' or c == 'knight'):
+    return 75
+  if p == 'foraging' and (c == 'archer' or c == 'thief'):
+    return 100
+  if p == 'foraging' and (c == 'pirate' or c == 'monk'):
+    return 100
+  if p == 'foraging' and (c == 'wizard' or c == 'priest'):
+    return 100
+  if p == 'fishing' and (c == 'warrior' or c == 'knight'):
+    return 70
+  if p == 'fishing' and (c == 'archer'):
+    return 70
+  if p == 'fishing' and (c == 'pirate' or c == 'monk' or c == 'thief'):
+    return 100
+  if p == 'fishing' and (c == 'wizard' or c == 'priest'):
+    return 100
+  return 150 # conservative floor price for advanced classes
+
+def get_genetics(statGenes):
+  from dfk.hero.utils import utils
+  # 1. decode statGenes into main/sub/prof D/R1/R2/R3
+  raw_genes = utils.__genesToKai(int(statGenes))
+  raw_genes = "".join(raw_genes.split(' '))
+  genetics = {'mainClass': {'D': "", 'R1': "", 'R2': "", 'R3': ""}, \
+              'subClass': {'D': "", 'R1': "", 'R2': "", 'R3': ""}, \
+              'profession': {'D': "", 'R1': "", 'R2': "", 'R3': ""}}
+  genetics['mainClass']['D']  = utils.parse_class(utils.__kai2dec(raw_genes[3]))
+  genetics['mainClass']['R1']  = utils.parse_class(utils.__kai2dec(raw_genes[2]))
+  genetics['mainClass']['R2']  = utils.parse_class(utils.__kai2dec(raw_genes[1]))
+  genetics['mainClass']['R3']  = utils.parse_class(utils.__kai2dec(raw_genes[0]))
+  genetics['subClass']['D']  = utils.parse_class(utils.__kai2dec(raw_genes[7]))
+  genetics['subClass']['R1']  = utils.parse_class(utils.__kai2dec(raw_genes[6]))
+  genetics['subClass']['R2']  = utils.parse_class(utils.__kai2dec(raw_genes[5]))
+  genetics['subClass']['R3']  = utils.parse_class(utils.__kai2dec(raw_genes[4]))
+  genetics['profession']['D']  = utils.parse_profession(utils.__kai2dec(raw_genes[11]))
+  genetics['profession']['R1']  = utils.parse_profession(utils.__kai2dec(raw_genes[10]))
+  genetics['profession']['R2']  = utils.parse_profession(utils.__kai2dec(raw_genes[9]))
+  genetics['profession']['R3']  = utils.parse_profession(utils.__kai2dec(raw_genes[8]))
+  return genetics
+
+def get_stuff(d, r):
+  for k in d:
+    if k == 'D':
+      r[d[k]] += 0.5 * 0.75
+    elif k == 'R1':
+      r[d[k]] += 0.5 * 0.20
+    elif k == 'R2':
+      r[d[k]] += 0.5 * 0.04
+    elif k == 'R3':
+      r[d[k]] += 0.5 * 0.01
+  return r
+
+def advanced(class1, class2):
+  if sorted([class1, class2]) == sorted(['warrior', 'knight']):
+    return 'paladin'
+  if sorted([class1, class2]) == sorted(['pirate', 'monk']):
+    return 'ninja'
+  if sorted([class1, class2]) == sorted(['archer', 'thief']):
+    return 'darkKnight'
+  if sorted([class1, class2]) == sorted(['priest', 'wizard']):
+    return 'summoner'
+  return None
+
+def get_children(genes1, genes2):
+  class_results = {}
+  prof_results = {}
+  for c in CLASSES:
+    class_results[c] = 0
+  for p in PROFESSIONS:
+    prof_results[p] = 0
+  class_results = get_stuff(genes1['mainClass'], class_results)
+  class_results = get_stuff(genes2['mainClass'], class_results)
+  prof_results = get_stuff(genes1['profession'], prof_results)
+  prof_results = get_stuff(genes2['profession'], prof_results)
+
+  for k1, v1 in enumerate(genes1):
+    for k2, v2 in enumerate(genes2):
+      adv_class = advanced(v1,v2)
+      if adv_class and class_results[adv_class] == 0:
+        class_results[adv_class] += 0.25 * (class_results[v1] + class_results[v2])
+        class_results[v1] *= 0.75
+        class_results[v2] *= 0.75
+
+  results = []
+  for c in class_results:
+    for p in prof_results:
+      results.append([c, p, gen1_floor(c,p), class_results[c] * prof_results[p]])
+
+  return results
+
+def main(hero1_id, hero2_id):
+  hero1 = HEROES[HEROES['id']==hero1_id]
+  hero2 = HEROES[HEROES['id']==hero2_id]
+  hero1_genes = get_genetics(hero1['statGenes'])
+  hero2_genes = get_genetics(hero2['statGenes'])
+  data = get_children(hero1_genes, hero2_genes)
+  results = pandas.DataFrame(data, columns = ['mainClass', 'profession', 'value', 'probability'])
+  print(hero1_id, hero2_id, (results['value'] * results['probability']).sum())
+#  print(results[results['probability'] != 0].sort_values('probability', ascending=False))
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description='Matchmaker. Enter hero ID and desired result')
+  parser.add_argument('--h1', help='input hero id', required=True)
+  parser.add_argument('--h2', help='input hero id', required=True)
+  args = vars(parser.parse_args())
+  main(args['h1'], args['h2'])
